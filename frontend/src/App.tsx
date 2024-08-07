889c654b44f74d4d1d04c072db13bdbf46126d03
meganbyrd22 <meganbyrd22@gmail.com>
2024-08-07 15:50:33 -0500

    feature: Add navbar component from Chakra

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,6 +3,7 @@ import React from 'react';
 //import logo from './logo.svg';
 import './App.css';
 import { ChakraBaseProvider } from './chakra-provider';
+import WithSubnavigation from './components/Navbar';
 
 // Define GraphQL query
 /*const MESSAGE_QUERY = gql`
@@ -23,20 +24,11 @@ function App() {
   return (
     <div className="App">
       <ChakraBaseProvider>
+        <WithSubnavigation />
       <header className="App-header">
         {/* Display the message fetched from the GraphQL query */}
         {/*<h1>{data.message}</h1> --> Remove comment to use GraphQL*/}
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+       
       </header>
       </ChakraBaseProvider>
     </div>
